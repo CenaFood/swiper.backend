59d08b8f9b20f297f69cb9b8e01e17d8cf767326
Philipp Lüthi <philipp@luethi.rocks>
2017-10-13 15:23:26 +0200

    added db Context as in https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro

---
+++ swiper.backend.db/SwiperContext.cs
@@ -0,0 +1,30 @@
+﻿using ch.cena.swiper.backend.db.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ch.cena.swiper.backend.db
+{
+    class SwiperContext: DbContext
+    {
+        public SwiperContext(DbContextOptions<SwiperContext> options) : base(options) { }
+
+        public DbSet<Annotation> Annotations { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Challenge> Challenges { get; set; }
+        public DbSet<ChallengeType> ChallengeTypes { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Annotation>().ToTable("Annotations");
+            modelBuilder.Entity<Answer>().ToTable("Answers");
+            modelBuilder.Entity<Challenge>().ToTable("Challenges");
+            modelBuilder.Entity<ChallengeType>().ToTable("ChallengeTypes");
+            modelBuilder.Entity<Project>().ToTable("Projects");
+            modelBuilder.Entity<User>().ToTable("Users");
+        }
+    }
+}
