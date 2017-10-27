c3089cad82bc2199d2c7fa88b03c1dd4e89899fd
Philipp Lüthi <philipp@luethi.rocks>
2017-10-27 14:35:47 +0200

    made initialMigration files

---
+++ swiper.backend.data/Migrations/20171027114434_InitialMigration.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ch.cena.swiper.backend.data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChallengeTypes",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChallengeTypes", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExpiryDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IssueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProjectDescription = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MailAddress = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Answers",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ChallengeTypeID = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Descripton = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Answers", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Answers_ChallengeTypes_ChallengeTypeID",
+                        column: x => x.ChallengeTypeID,
+                        principalTable: "ChallengeTypes",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Challenges",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ChallengeTypeID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Payload = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProjectID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Challenges", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Challenges_ChallengeTypes_ChallengeTypeID",
+                        column: x => x.ChallengeTypeID,
+                        principalTable: "ChallengeTypes",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Challenges_Projects_ProjectID",
+                        column: x => x.ProjectID,
+                        principalTable: "Projects",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Annotations",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AnswerID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ChallengeID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Payload = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Annotations", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Annotations_Answers_AnswerID",
+                        column: x => x.AnswerID,
+                        principalTable: "Answers",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Annotations_Challenges_ChallengeID",
+                        column: x => x.ChallengeID,
+                        principalTable: "Challenges",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Annotations_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Annotations_AnswerID",
+                table: "Annotations",
+                column: "AnswerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Annotations_ChallengeID",
+                table: "Annotations",
+                column: "ChallengeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Annotations_UserID",
+                table: "Annotations",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_ChallengeTypeID",
+                table: "Answers",
+                column: "ChallengeTypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Challenges_ChallengeTypeID",
+                table: "Challenges",
+                column: "ChallengeTypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Challenges_ProjectID",
+                table: "Challenges",
+                column: "ProjectID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Annotations");
+
+            migrationBuilder.DropTable(
+                name: "Answers");
+
+            migrationBuilder.DropTable(
+                name: "Challenges");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "ChallengeTypes");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
