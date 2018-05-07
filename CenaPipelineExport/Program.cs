62a709e4f6007e8d808e7743b722a22321075aeb
Philipp Lüthi <philipp@luethi.rocks>
2018-05-07 14:32:48 +0200

    Added console argument parsing option.

--- CenaPipelineExport/Program.cs
+++ CenaPipelineExport/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using ch.cena.swiper.backend.data;
+using ch.cena.swiper.backend.data.Models;
+using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Linq;
 
 namespace CenaPipelineExport
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var app = new CommandLineApplication();
+
+            app.HelpOption();
+            var trainRatioOption = app.Option("-t|--trainRatio <ratio>", "The ratio for train data", CommandOptionType.SingleValue);
+            var overrideOption = app.Option("-o", "If set all outputs will be overwritten.", CommandOptionType.NoValue);
+
+            app.OnExecute(() =>
+            {
+                var trainRatio = trainRatioOption.HasValue()
+                    ? float.Parse(trainRatioOption.Value())
+                    : 0.8;
+
+
+                //TODO: Overrideoption
+                Console.WriteLine($"Train ratio {trainRatio}!");
+
+
+
+                var testRatio = 1 - trainRatio;
+
+
+
+
+                return 0;
+            });
+
+            return app.Execute(args);
         }
     }
 }
