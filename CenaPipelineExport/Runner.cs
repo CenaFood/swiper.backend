ccf81765f897f575d9f7a02969b1505da3de6a18
Philipp Lüthi <philipp@luethi.rocks>
2018-05-14 12:41:30 +0200

    Addend DI and configuration. New approach to use existing Service and database.

---
+++ CenaPipelineExport/Runner.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CenaPipelineExport
+{
+    class Runner
+    {
+        private readonly ILogger<Runner> _logger;
+
+        public Runner(ILogger<Runner> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Export()
+        {
+            _logger.LogDebug(20, "Stated export");
+        }
+
+    }
+}
